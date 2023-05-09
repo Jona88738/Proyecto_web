30ec974a5dbd389642c0281fb989e2fe13747708
Jonathan Perez <jona88738@gmail.com>
2023-05-08 23:51:30 -0600

    Avance de login

---
+++ controladores/login.php
@@ -0,0 +1,17 @@
+<?php
+
+    class Login extends Controlador{
+
+        function __construct(){
+
+            parent::__construct();
+        }
+
+        public function render(){
+            $this->view->render('login');
+        }
+    }
+
+
+
+?>
